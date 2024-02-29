7a2db992559270ff23dd27badafb03e3fbb0bb35
Henrique Paixao <henriquemfpaixao@gmail.com>
2024-02-29 20:27:45 -0300

    amazon da shopee

--- fundamentos-react-01-henrique-paixao/src/App.js
+++ fundamentos-react-01-henrique-paixao/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import dadosJason from './dados.json'
 import './App.css';
-import Caverna from './livros/cavernas_aco.jpg'
+import Livro from './components/livros';
+
 
 function App() {
   return (
   <div className='App'>
-    <div className="card-book">
-      <h3>As Cavernas de Aço</h3>
-      <p>Isaac Asimov</p>
-      <img src={Caverna} width='200'  alt='As cavernas de aço'></img>
-      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs,
-      uma das mais populares da ficção científica. A história nasceu de um desafio:
-      Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, 
-      inclusive nos dramas policiais. A obra foi publicada, pela primeira vez, na revista Galaxy Science Fiction, 
-      e o sucesso foi tão grande que, um ano depois, se tornou livro. Na trama, Os Mundos Siderais, 
-      antigas colônias da Terra, possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas. 
-      Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra, 
-      levando os humanos a perderem seus empregos e privilégios de cidadãos.</p>
-    </div>
+      <Livro dadosJson={dadosJason}></Livro>
   </div>
   );
 }
